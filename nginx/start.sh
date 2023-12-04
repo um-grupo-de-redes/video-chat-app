fbfc45a345243e99a780a0e3ed79bd64f7ea1789
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2023-12-04 14:28:16 -0300

    server deploy with SSL certificate

---
+++ nginx/start.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+FULLCHAIN=/etc/nginx/ssl/live/${SERVER_NAME}/fullchain.pem
+PRIVKEY=/etc/nginx/ssl/live/${SERVER_NAME}/privkey.pem
+
+# Delete the default.conf file
+if [ -f "/etc/nginx/conf.d/default.conf" ] ; then
+    rm "/etc/nginx/conf.d/default.conf"
+fi
+
+# Substitute placeholders with current environment variables
+export DOLLAR="$"
+
+envsubst < /etc/nginx/conf.d/01_http.conf.bak | tee /etc/nginx/conf.d/01_http.conf.bak.bak > /dev/null 2>&1
+envsubst < /etc/nginx/conf.d/02_https.conf.bak | tee /etc/nginx/conf.d/02_https.conf.bak.bak > /dev/null 2>&1
+
+# Enable "/.well-known/acme-challenge/" endpoint
+cp /etc/nginx/conf.d/01_http.conf.bak.bak /etc/nginx/conf.d/01_http.conf
+echo "'/.well-known/acme-challenge/' endpoint enabled."
+
+nginx -g "daemon off;" &
+
+# Wait for SSL certificates before enabling HTTPS
+until [ -f ${FULLCHAIN} ]
+do
+     sleep 1
+done
+
+until [ -f ${PRIVKEY} ]
+do
+     sleep 1
+done
+
+# Wait for frontend build folder before enabling the web app
+until [ -f ${FRONTEND_BUILD}/index.html ]
+do
+     sleep 1
+done
+sleep 5
+
+# Enable 02_https.conf
+cp /etc/nginx/conf.d/02_https.conf.bak.bak /etc/nginx/conf.d/02_https.conf
+nginx -s reload
+
+echo "Enabled 02_https.conf."
+
+# Sleeps while keeping the ssl certificates up to date
+trap exit TERM
+
+while true
+do
+    # The SSL certificates are renewed by certbot about 30 days before the expire date,
+    # so it should be okay to reload nginx in 0h to 24h after the renew.
+    sleep 24h & wait ${!}
+    nginx -s reload
+done
